aa9fcb48eb77ff1d972a4d892b40b66a1b586fe0
Hariharan217 <harikalai217@gmail.com>
2024-03-18 22:17:19 +0530

    portfolio01

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 
-
-let express = require('express');
+let express = require('express')
+const nodemailer = require("nodemailer")
 let cors =require('cors')
+
 let app = express();
+
 app.use(express.json())
+
 app.use(cors())
 
 
-let userRoute = require('./Routes/Passwordroute')
+app.post('/', async (req, res)=>{
 
+    let {name,mail,mobile,mailsubject,message} = req.body
+       
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "harikalai217@gmail.com",
+                pass: "phmovtswvtpxbbef"
+            },
+        });
+    
+        var mailOptions = {
+            from: mail,
+            to:"harikalai217@gmail.com" ,
+            subject: `${mailsubject} - from RESUME`,
+            text: `
+            Name : ${name},
+            mobile : ${mobile}
+            Message : ${message}`
+        };
+    
+        transporter.sendMail(mailOptions, (error) => {
+            if (error) {
+                res.send('Error: ', error)
+            }
+            else {
+                res.status(200).send("Email sent") ;
+            };
+        });
+    }
+  
 
-
-app.use('/user', userRoute)
-
+)
 
 
 app.listen(5001);
